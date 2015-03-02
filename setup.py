df8b6d67a5407bbb0f965faa1eedbb6c4fdb7464
Sander van de Graaf <mail@svdgraaf.nl>
2015-03-02 15:20:08 +0100

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except:
 
 setup(
     name = 'scratch-ext',
-    version = '0.5.10',
+    version = '0.5.11',
     description='Access external Scratch sensors and endpoints through python',
     long_description=README,
     author = 'Sander van de Graaf',
