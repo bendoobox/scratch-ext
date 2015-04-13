bad3fc7552055aa31a4f29ff0af71c337de25e7e
Sander Van de Graaf <sander.vandegraaf@combellgroup.com>
2015-04-13 13:59:06 +0200

    added i2c plugin

--- scratch-ext/plugins/i2c.py
+++ scratch-ext/plugins/i2c.py
@@ -20,7 +20,7 @@ except:
 class I2CPlugin(PluginBase):
     commands = ['i2c',]
     socket = None
-    addresses = []
+    devices = []
 
     def __init__(self, socket):
         logger.debug("Initializing...")
@@ -40,27 +40,30 @@ class I2CPlugin(PluginBase):
         # check if we need to process this command
         if any(command in message for command in self.commands):
             logger.debug('acting on: %s' % message)
-            message = message.replace('broadcast ', '')
+            message = message.strip().replace('broadcast ', '')
             message = message.replace('"','')
             message = message.lower()
             matches = re.search('(i2c) ?(?P<device>[0-9]+) (?P<address>[0-9]+).?(?P<value>on|1|off|0|high|low)?', str(message.strip()))
             if matches:
-                self.i2c(device=int(matches.groupdict(0)['device']), address=matches.groupdict(0)['address'], value=matches.groupdict(0)['value'])
+                device = int(str(matches.groupdict(0)['device']), 16)
+                address = int(str(matches.groupdict(0)['address']), 16)
+                self.i2c(device=device, address=address, value=matches.groupdict(0)['value'])
+            else:
+                logger.debug('no match: %s', message.strip())
         else:
             logger.debug('ignoring: %s' % message.strip())
 
     def tick(self):
-        if not supported:
+        if not supported or len(self.devices) == 0:
             return False
         # Loop through all device address which were read, and update
-        for device, address in self.addresses.items():
+        for device, address in self.devices:
             value = self.i2c(device, address)
-            cmd = 'sensor-update "i2c %s %s" %s' % (device, address, value)
+            cmd = 'sensor-update "i2c %s %s" %s' % (format(device, 'x'), format(address, 'x'), value)
             self.send(cmd)
+            #time.sleep(0.1)
 
     def i2c(self, device, address, value=None):
-        foo = int(str(device), 16)
-        address = int(address)
         if not supported:
             return None
         if value:
@@ -72,9 +75,14 @@ class I2CPlugin(PluginBase):
                 return False
         else:
             # read
-            value = bus.read_byte_data(foo, address)
-            time.sleep(0.5)
-            self.devices.append((device, address))
-            return value
-            
+            logging.debug('reading: %s %s' % (device, address))
+            # Write to the address
+            bus.write_byte_data(device, address, 0)
+            # And read the output
+            value = bus.read_byte(device)
 
+            # if the device + address is not listed yet, add it to the output
+            if (device, address) not in self.devices:
+                self.devices.append((device, address))
+
+            return value
