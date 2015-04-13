f5eacfee40f63e218d17368e5b7ab888abf52fe2
Sander Van de Graaf <sander.vandegraaf@combellgroup.com>
2015-04-13 14:02:38 +0200

    new version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except:
 
 setup(
     name = 'scratch-ext',
-    version = '0.6.1',
+    version = '0.7.5',
     description='Access external Scratch sensors and endpoints through python',
     long_description=README,
     author = 'Sander van de Graaf',
